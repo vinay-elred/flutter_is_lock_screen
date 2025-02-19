84757ec7cd8972e6365e1820bf9afc430be4acff
Vinay_Sharma <vinay@elred.io>
2025-02-19 16:44:23 +0530

    fixes v2 android binding

--- android/src/main/kotlin/com/chihimng/is_lock_screen/IsLockScreenPlugin.kt
+++ android/src/main/kotlin/com/chihimng/is_lock_screen/IsLockScreenPlugin.kt
@@ -4,75 +4,45 @@ import android.app.KeyguardManager
 import android.content.Context
 import android.os.Build
 import android.os.PowerManager
-import androidx.annotation.NonNull;
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-/** IsLockScreenPlugin */
-public class IsLockScreenPlugin(val registrarContext: Context? = null) : FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-  private var bindingContext : Context? = null
+class IsLockScreenPlugin : FlutterPlugin, MethodCallHandler {
+  private lateinit var channel: MethodChannel
+  private lateinit var context: Context
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    bindingContext = flutterPluginBinding.applicationContext
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "is_lock_screen")
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    context = binding.applicationContext
+    // Use binaryMessenger instead of dartExecutor
+    channel = MethodChannel(binding.binaryMessenger, "is_lock_screen")
     channel.setMethodCallHandler(this)
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "is_lock_screen")
-      channel.setMethodCallHandler(IsLockScreenPlugin(registrarContext = registrar.activeContext()))
-    }
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: Result) {
     when (call.method) {
       "isLockScreen" -> {
-        val context = bindingContext ?: registrarContext
-        ?: return result.error("NullContext", "Cannot access system service as context is null", null)
-
-        val keyguardManager: KeyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
-        val inKeyguardRestrictedInputMode: Boolean = keyguardManager.inKeyguardRestrictedInputMode()
-
+        val keyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        val inKeyguardRestrictedInputMode = keyguardManager.inKeyguardRestrictedInputMode()
         val isLocked = if (inKeyguardRestrictedInputMode) {
           true
         } else {
-          val powerManager: PowerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+          val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
             !powerManager.isInteractive
           } else {
             !powerManager.isScreenOn
           }
         }
-        return result.success(isLocked)
+        result.success(isLocked)
       }
-      else -> {
-        return result.notImplemented()
-      }
+      else -> result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
 }
